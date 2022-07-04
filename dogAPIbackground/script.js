69fb69791089fb272d53ddce633dae227d8bcdfe
hao759 <qhao74154@gmail.com>
2022-07-04 21:05:45 +0700

    api hero

--- dogAPIbackground/script.js
+++ dogAPIbackground/script.js
@@ -1,6 +1,8 @@
+
 const API="https://dog.ceo/api/breeds/image/random";
 const dogImage=document.getElementById('dogImage');
 const getDog=document.getElementById('getDog');
+
 
 fetch(API).then(res=>res.json())
     .then(json =>dogImage.innerHTML=`<img src='${json.message}' height=300 width=300/>`)
---
+++ superHeroAPI/scripp.js
@@ -0,0 +1,22 @@
+
+const API="https://www.superheroapi.com/api.php/1830045643997167";
+const hero=document.getElementById('hero');
+const getHero=document.getElementById('getHero');
+var randomm=18;
+
+fetch(`${API}/${randomm}`).then(res=>res.json())
+     .then(json =>hero.innerHTML =`<h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
+
+const getsuperHero=(random)=>{
+     console.log(`${API}/${random}`);
+
+    fetch(`${API}/${random}`).then(res=>res.json())
+     .then(json =>hero.innerHTML =`<h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
+    //.then(json =>console.log(`${json.image.url}"`));
+}
+
+
+getHero.onclick=()=>{
+    randomm=Math.floor(Math.random() * 731);
+    getsuperHero(randomm);
+    }
