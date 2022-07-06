fea97c031f5f2f4391b0d5e885c6595f3df189ee
hao759 <qhao74154@gmail.com>
2022-07-06 07:45:53 +0700

    test

--- superHeroAPI/scripp.js
+++ superHeroAPI/scripp.js
@@ -5,18 +5,19 @@ const getHero=document.getElementById('getHero');
 var randomm=18;
 
 fetch(`${API}/${randomm}`).then(res=>res.json())
-     .then(json =>hero.innerHTML =`<h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
+     .then(json =>hero.innerHTML =`Name: <h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
 
 const getsuperHero=(random)=>{
      console.log(`${API}/${random}`);
 
     fetch(`${API}/${random}`).then(res=>res.json())
-     .then(json =>hero.innerHTML =`<h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
+     .then(json =>hero.innerHTML =`Name: <h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
     //.then(json =>console.log(`${json.image.url}"`));
 }
 
 
+
 getHero.onclick=()=>{
-    randomm=Math.floor(Math.random() * 731);
+    randomm=Math.floor(Math.random() * 730 +1);
     getsuperHero(randomm);
     }
