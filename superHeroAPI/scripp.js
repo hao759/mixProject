30fcbbb6fe7470afbd4a980f8727c1f77d2f2bbf
hao759 <qhao74154@gmail.com>
2022-12-12 19:18:39 +0700

    add chat bot

--- superHeroAPI/scripp.js
+++ superHeroAPI/scripp.js
@@ -1,23 +1,27 @@
+const API = "https://www.superheroapi.com/api.php/1830045643997167";
+const hero = document.getElementById("hero");
+const getHero = document.getElementById("getHero");
+var randomm = 18;
 
-const API="https://www.superheroapi.com/api.php/1830045643997167";
-const hero=document.getElementById('hero');
-const getHero=document.getElementById('getHero');
-var randomm=18;
+fetch(`${API}/${randomm}`)
+  .then((res) => res.json())
+  .then(
+    (json) =>
+      (hero.innerHTML = `Name: <h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`)
+  );
 
-fetch(`${API}/${randomm}`).then(res=>res.json())
-     .then(json =>hero.innerHTML =`Name: <h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
+const getsuperHero = (random) => {
+  console.log(`${API}/${random}`);
 
-const getsuperHero=(random)=>{
-     console.log(`${API}/${random}`);
-
-    fetch(`${API}/${random}`).then(res=>res.json())
-     .then(json =>hero.innerHTML =`Name: <h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`);
-    //.then(json =>console.log(`${json.image.url}"`));
-}
-
-
-
-getHero.onclick=()=>{
-    randomm=Math.floor(Math.random() * 730 +1);
-    getsuperHero(randomm);
-    }
+  fetch(`${API}/${random}`)
+    .then((res) => res.json())
+    .then(
+      (json) =>
+        (hero.innerHTML = `Name: <h1 style=" white-space: nowrap;">${json.biography["full-name"]}</h1><img src="${json.image.url}"/>`)
+    );
+  //.then(json =>console.log(`${json.image.url}"`));
+};
+getHero.onclick = () => {
+  randomm = Math.floor(Math.random() * 730 + 1);
+  getsuperHero(randomm);
+};
